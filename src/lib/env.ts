858bc468e8de4b558d7c89f86b37cf2bfd531118
Jorge Bierhals Ramos <jorginhodeveloper@gmail.com>
2025-01-25 15:30:46 -0300

    chore: update API URL configuration for client-side environment

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -2,10 +2,10 @@ import { z } from "zod";
 import { createEnv } from "@t3-oss/env-nextjs";
 
 export const env = createEnv({
-  server: {
-    APP_URL: z.string().url(),
+  client: {
+    NEXT_PUBLIC_API_URL: z.string().url(),
   },
   runtimeEnv: {
-    APP_URL: process.env.APP_URL,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
 });
--- src/lib/http-client.ts
+++ src/lib/http-client.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 import { env } from "@/lib/env";
 
 export const httpClient = axios.create({
-  baseURL: env.APP_URL,
+  baseURL: env.NEXT_PUBLIC_API_URL,
 });
