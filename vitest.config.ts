3daecb2ddc4ada26d7ddb1d10cf3c75e60e44fd5
Jorge Bierhals Ramos <jorginhodeveloper@gmail.com>
2025-01-26 21:05:03 -0300

    chore: add Vitest V8 coverage configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,9 +4,6 @@ import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-  },
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
