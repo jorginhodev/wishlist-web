ac90bc60576fabe2580be843c1503a5a33db10ad
Jorge Bierhals Ramos <jorginhodeveloper@gmail.com>
2025-01-26 21:41:19 -0300

    feat: update root layout with header and providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,30 @@
-import type { Metadata } from "next";
+// eslint-disable-next-line camelcase
 import { Source_Sans_3 } from "next/font/google";
+import { Providers } from "./providers";
 import "./globals.css";
-
-export const metadata: Metadata = {
-  title: "Wishlist",
-  description: "Wishlist de produtos",
-};
+import { Header } from "@/components/header";
+import { Metadata } from "next";
 
 const sourceSans = Source_Sans_3({ subsets: ["latin"] });
 
+export const metadata: Metadata = {
+  title: "Wishlist",
+  description: "Wishlist app for your favorite products",
+};
+
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body className={`${sourceSans.className} antialiased`}>{children}</body>
+      <body className={sourceSans.className}>
+        <Providers>
+          <Header />
+          <main className="h-screen w-full">{children}</main>
+        </Providers>
+      </body>
     </html>
   );
 }
